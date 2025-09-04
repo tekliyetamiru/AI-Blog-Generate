8ed3037a7a66301366a0162cff21a221620a9397
tekliyetamiru <tekliyetamru@gmail.com>
2025-09-04 15:37:00 +0300

    create signup,login and logout in django app

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('login',views.user_login,name='login'),
+    path('signup',views.user_signup,name='signup'),
+    path('logout',views.user_logout,name='logout'),
+    path('',views.index,name='index'),
 ]
