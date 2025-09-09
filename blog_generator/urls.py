3a60597709becc369aa4ecf611f2b35c50e138f8
tekliyetamiru <tekliyetamru@gmail.com>
2025-09-09 20:20:10 +0300

    add some functionality for views

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login',views.user_login,name='login'),
     path('signup',views.user_signup,name='signup'),
     path('logout',views.user_logout,name='logout'),
-    path('',views.index,name='index'),
+    path('generate-blog',views.generate_blog,name='generate-blog'),
 ]
--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,12 +1,25 @@
+from django.http import JsonResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 @login_required
 def index(request):
     return render(request,'index.html')
-
+@csrf_exempt
+def generate_blog(request):
+    if request.method == "POST":
+        try:
+            data= json.loads(request.body)
+            yt_link = data['link']
+            return JsonResponse({'content':yt_link})
+        except(KeyError,json.JSONDecodeError):
+            return JsonResponse({'error':'Invalid data sent'},status=400)
+    else:
+        return JsonResponse({'error':'Invalid request method'},status=405)
 def user_login(request):
     if request.method == "POST":
         username=request.POST['username']
@@ -15,7 +28,7 @@ def user_login(request):
         user=authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
-            return redirect('/')
+            return redirect('/')     
         else:
             error_message="Invalid username or password"
             return render(request,'login.html',{'error_message':error_message})
